e6b00ac16c3ad66d3ff5ab6696019bcffe288e84
Administrator <Administrator@PC244>
2020-06-20 03:59:52 +0700

    attendance

---
+++ HoDuyThuan_Lab456/Controllers/AttendancesController.cs
@@ -0,0 +1,22 @@
+﻿using HoDuyThuan_Lab456.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HoDuyThuan_Lab456.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        
+    }
+}
